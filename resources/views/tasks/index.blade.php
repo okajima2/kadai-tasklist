c8a7322aedb660068b9b50fb1e4ddc27df154cb9
takaaki.okajima <t.okajima2@gmail.com>
2020-06-14 02:06:14 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク一覧</h1>
+    
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>タスク</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tasks as $task)
+            <tr>
+                <td>{{ $task->id }}</td>
+                <td>{!! link_to_route('tasks.show', $task->content, ['task' => $task->id]) !!}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
+    {{-- タスク作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-primary']) !!}
+
+@endsection
